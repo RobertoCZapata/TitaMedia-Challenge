4649029ab52d8c08027d51268e2ee21c04781250
Roberto Carlos Zapata Reyes <robertozapataz25@gmail.com>
2020-08-30 20:15:14 -0500

    get JSON from unsplash API

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function request() {
+    fetch("https://api.unsplash.com/photos/?client_id=H2YLbBoGNtbqydP5k-bwy4wY8IwOYTGkBte_H318Ztw")
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);    
+        data.forEach(item => {
+            console.log(item);
+        });
+    })
+}
+
+
 document.querySelector('.menu-btn').addEventListener('click', () => {
    document.querySelector('.nav-menu').classList.toggle('show');
 })
